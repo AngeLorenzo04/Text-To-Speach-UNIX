96ccb79db65e0e85d7b8a07e43e4263753f5a60d
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2026-06-01 15:53:11 +0200

    style: simbolo ▶ nella notifica in stile ⏹ di stop.sh

--- read.sh
+++ read.sh
@@ -94,6 +94,6 @@ echo "$BGPID" > "$PID_FILE"
 
 # Notifica immediata: l'audio parte dopo qualche secondo (download rete),
 # così l'utente sa che la shortcut è stata registrata correttamente.
-notify-send -i media-playback-start -t 3000 "🔊 TTS" "Lettura in avvio..." 2>/dev/null
+notify-send -t 3000 "▶ TTS" "Lettura in avvio..." 2>/dev/null
 
 echo "▶ Lettura avviata (PID: $BGPID, voce: $VOICE)"
